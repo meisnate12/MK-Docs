a57a7ab278421399471274091ad9f1840ce4ad94
YozoraXCII <96386153+YozoraXCII@users.noreply.github.com>
2023-10-31 14:12:46 +0000

    change tabs based on branch

--- docs/stylesheets/branch.js
+++ docs/stylesheets/branch.js
@@ -7,6 +7,7 @@ function checkURLForBranch() {
   if (currentURL.indexOf("config") !== -1) {
     // If "config" is found in the URL, change the CSS of .md-header to red
     document.querySelector(".md-header").style.backgroundColor = "#ef5552";
+    document.querySelector(".md-tabs").style.backgroundColor = "#ef5552";
 
     // Change the text of <span class="md-ellipsis">
     const ellipsisSpan = document.querySelector(".md-ellipsis");
@@ -16,6 +17,7 @@ function checkURLForBranch() {
   } else if (currentURL.indexOf("metadata") !== -1) {
     // If "metadata" is found in the URL, change the CSS of .md-header to yellow
     document.querySelector(".md-header").style.backgroundColor = "#ffa724";
+    document.querySelector(".md-tabs").style.backgroundColor = "#ffa724";
 
     // Change the text of <span class="md-ellipsis">
     const ellipsisSpan = document.querySelector(".md-ellipsis");
